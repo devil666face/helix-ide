81e89759e64c1780bae9b7f1c9e1a3ca2979e284
Devil666face <artem1999k@gmail.com>
2023-10-27 22:44:42 +0300

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter your main user " NON_ROOT_USER
 cd /opt
-wget https://github.com/Devil666face/helix-ide/releases/latest
+wget https://github.com/Devil666face/helix-ide/releases/latest/download/helix-ide.tgz
 rm -rf helix && tar -xf helix-ide.tgz && rm -rf helix-ide.tgz
 chown -R $NON_ROOT_USER:$NON_ROOT_USER ./helix
 cd helix
